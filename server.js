65c4bf7f7c505e2d7b10e520574c58ea6689898f
ShengzeWang <w210@outlook.it>
2022-03-31 11:38:02 -0500

    1.3

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(express.static(viewsDir))
 // Redirect the Pages
 app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'index.html')))
 app.get('/realtime_face_expression_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'realtime_face_expression_recognition.html')))
-app.get('/face_expression_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'face_expression_recognition_index.html')))
+app.get('/about', (req, res) => res.sendFile(path.join(viewsDir, 'about.html')))
+app.get('/privacy', (req, res) => res.sendFile(path.join(viewsDir, 'privacy.html')))
+app.get('/manual', (req, res) => res.sendFile(path.join(viewsDir, 'manual.html')))
 
 // Lauch the Webapp on Port 3001
 app.listen(3001, () => console.log('http://localhost:3001/ or https://deepemo.tech/'))
